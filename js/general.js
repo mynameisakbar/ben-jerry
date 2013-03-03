359d13bd16418265b8335cc2bed7abdf460b5b52
mynameisakbar <akbar_pahlevi@yahoo.com>
2013-03-03 17:51:50 +0800

    image uploading to canvas with base64 conversion with basic styling

--- js/general.js
+++ js/general.js
@@ -1,11 +1,64 @@
-<!DOCTYPE HTML>
-<html>
+window.onload = function() {
 
-<head>
-	<title>B&J</title>
-</head>
+  setTimeout(function(){
+    // Hide the address bar!
+    window.scrollTo(0, 1);
+  }, 0);
 
-<body>
-</body>
+  var fileInput = document.getElementById('mainInput');
+  fileInput.onchange = function() {
+    var file = fileInput.files[0];
 
-</html>
+    var abc = new FileReader();
+
+    abc.readAsBinaryString(file);
+    abc.onload = function(){
+
+        var finalExif = EXIF.readFromBinaryFile(new BinaryFile(abc.result));
+        //alert(finalExif.Orientation);
+        var mpImg = new MegaPixImage(file);
+        var imgOrient = 1;
+
+        if(finalExif.Orientation == 6){
+            //alert("rotate it");
+            imgOrient = 6;
+            //alert("now change it to" + imgOrient);
+        }else{
+            imgOrient = 1;
+        }
+
+        console.log(mpImg);
+        console.log(mpImg.srcImage);
+
+         //Render resized image into canvas element.
+        var resCanvas1 = document.getElementById('canvasImage');
+        console.log(resCanvas1);
+        mpImg.render(resCanvas1, { maxWidth: 180, maxHeight: 190, quality:1, orientation: imgOrient});
+
+    }
+
+    /*abc.onloadend = function(){
+        console.log($('#canvasImage').height());
+        if($('#canvasImage').width() < 200){
+            console.log("less than 200");
+            //$('#resultCanvas').css("left", "45px");
+        }else{
+            //$('#resultCanvas').css("left", "10px");
+        }
+    }
+
+    (function checkCanvas() {
+        console.log($('#canvasImage').height());
+        if($('#canvasImage').width() < 200){
+            console.log("less than 200");
+            //$('#resultCanvas').css("left", "45px");
+        }else{
+            //$('#resultCanvas').css("left", "10px");
+        }
+    }*/
+    
+    console.log(file);
+    console.log(file.type);
+    
+  };
+};
