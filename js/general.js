78e5fc18d9cf1c487b4f8193626ac2e579f9e768
mynameisakbar <akbar_pahlevi@yahoo.com>
2013-03-04 18:23:46 +0800

    form animation done, left with field styling

--- js/general.js
+++ js/general.js
@@ -45,24 +45,38 @@ window.onload = function() {
     
   };
 
-  var element = document.getElementById('continue-btn');
-  var hammertime = Hammer(element).on("tap", function(event) {
-        //console.log('tap me bro!');
-
-        
+  var hammertime = $("#continue-btn").hammer();
+  hammertime.on("tap", function(ev) {
+  console.log($('#startFriend').offset().left);
 
         //this condition for the 1st tap
-        if($('#choosePhoto').offset().left > 0){
-            console.log($('#choosePhoto').offset().left);
-            console.log($('#choosePhoto').offset().top);
-            //$("#choosePhoto").animate({"left":"-240px"},200);
-            //$("#mainInput").animate({"left":"-240px"},200);
-        }else{
-            console.log("it's not in bro");
-            //$('#canvasImage').css("padding-left", "35px");
+        if($('#canvasImage').width() > 200){
+            //console.log("it's not in bro");
+            alert("Please select an image to proceed");
+
+        }else if ($('#choosePhoto').offset().left > 0 && $('#canvasImage').width() < 200){
+            //console.log("ok bro");
+            $("#choosePhoto").animate({"left":"-240px"},200);
+            $("#mainInput").animate({"left":"-240px"},200);
+            $("#question-2-title").animate({"left":"0px"},200);
+            $("#startFriend").animate({"left":"0px"},200);
+
+        }else if($('#startFriend').offset().left > 0 && $('#startFriend').offset().left < 320 && $("#startFriend").val().length !=0){
+            console.log($("#startFriend").val());
+            //console.log("it's in bro");
+            $("#question-2-title").animate({"left":"-240px"},200);
+            $("#startFriend").animate({"left":"-240px"},200);
+            $("#question-3-title").animate({"left":"0px"},200);
+            $("#q-holder").animate({"left":"0px"},200);
+
+        }else if($("#name1").val().length !=0 && $("#name2").val().length !=0){
+            console.log("submit form");
+
         }
 
-    });
+  });
+
+        
 
 
 };
